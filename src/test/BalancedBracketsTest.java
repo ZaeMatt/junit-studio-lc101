9b67c8ec8950af627e0eefc4da18c2c78975477c
Zahon Matthews <zaematthews@gmail.com>
2022-08-04 20:43:18 -0400

    tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,6 +1,8 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
+
 
 import static org.junit.Assert.*;
 
@@ -10,6 +12,37 @@ public class BalancedBracketsTest {
     @Test
     public void emptyTest() {
         assertEquals(true, true);
+    }
+
+
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+
+
+
+    @Test
+    public void checkBracketsAreFalse() {
+
+        assertFalse(BalancedBrackets.hasBalancedBrackets ("["));
+    }
+
+    @Test
+    public void containsExclamation() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("!"));
+    }
+
+    @Test
+    public void containsQuestionMark() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("?"));
+    }
+
+    @Test
+    public void checkStringsWithoutGoodBrackets () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[code["));
     }
 
 
